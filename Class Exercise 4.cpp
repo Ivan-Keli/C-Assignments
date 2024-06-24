7c1827d47863dc83048018a9e805385b28f7ad4d
Ivan Mwendwa Keli <116259292+Ivan-Keli@users.noreply.github.com>
2024-06-24 21:22:43 +0300

    Add files via upload

---
+++ Class Exercise 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "\t\t\t\t\t*****Escape Sequence Practice*****\t\t\t\t\t" << endl;
+    cout << "BBIT is a unviersity diploma designed to focus on: " << endl;
+    cout << "1. Development" << endl;
+    cout << "2. Implementation, and" << endl;
+    cout << "3. Management of information systems." << endl;
+}
---
+++ Class Exercise.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Welcome to the wonderful world of C++! \n";
+    cout << "This is my first C++ output statement.\n";
+    cout << "Soon, I will learn how to input data into my program.\n";
+    cout << "Strathmore University is the leading university in the region. \n";
+}
